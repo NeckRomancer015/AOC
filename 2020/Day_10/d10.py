c5962e29fe79626fedb912d0897aaedf89978804
Shankar <49676416+NeckRomancer015@users.noreply.github.com>
2024-04-04 23:39:07 +0200

    2020 day 10 p2

--- 2020/Day_10/d10.py
+++ 2020/Day_10/d10.py
@@ -8,25 +8,40 @@ def getInput(InputPath:str)->List[int]:
         file.close()
     return Input
 
+def p2(Input: List[int]) ->None:    
+    Input.insert(0,0)
+    totalVariations = 0    
+    n = len(Input)
+    totalVariations = [0] * n
+    totalVariations[0] = 1  
+
+    for i in range(1, n):
+        for j in range(i):
+            if Input[i] - Input[j] <= 3:
+                totalVariations[i] += totalVariations[j]
+
+    print("Part 2 Answer is: ", totalVariations[n-1])
+
+
 def p1(Input : List[int]) -> None:
     Input.sort()
     diff = [0,1]
     currentV = 0
     for i in Input:
         if i -currentV == 1:
-            diff[0]=diff[0]+1
+            diff[0]+=1
         elif i- currentV == 3:
-            diff[1]=diff[1]+1
+            diff[1]+=1
         else:
             print("Wrong")
         currentV = i
     
     print(diff[0]*diff[1])
-
-
+    
 def main():
     Input = getInput(r'2020\Day_10\input.txt')
     p1(Input=Input)
+    p2(Input=Input)
 
 if __name__ == '__main__':
     main()
