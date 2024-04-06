e8210b99a378459087aba80e18e4fb6cb0bb888c
Shankar <49676416+NeckRomancer015@users.noreply.github.com>
2024-04-07 00:19:51 +0200

    2020 day 12 p2

--- 2020/Day_12/d12.py
+++ 2020/Day_12/d12.py
@@ -20,16 +20,36 @@ class ship():
         print(x,"+",y)
         return x+y
 
-def move(ferry:ship, action:str, number:int):
-    match ferry.direction:
+    def setAbsValues(self)->None:
+        if self.N >0 and self.S>0:
+            if self.N>self.S:
+                self.N-=self.S
+                self.S=0
+            elif self.S>self.N:
+                self.S-=self.N
+                self.N=0
+            else:
+                self.S,self.N=0,0
+        if self.E>0 and self.W>0:
+            if self.E>self.W:
+                self.E-=self.W
+                self.W=0
+            elif self.W>self.E:
+                self.W-=self.E
+                self.E=0
+            else:
+                self.W,self.E = 0,0
+
+def move(boat:ship, action:str, number:int):
+    match boat.direction:
         case 'E':
-            ferry.E +=number
+            boat.E +=number
         case 'W':
-            ferry.W+=number
+            boat.W+=number
         case 'S':
-            ferry.S+=number
+            boat.S+=number
         case 'N':
-            ferry.N+=number
+            boat.N+=number
 
 def rotate(ferry:ship, action: str, number:int)->None:
     nesw = ['N','E','S','W']
@@ -43,7 +63,7 @@ def rotate(ferry:ship, action: str, number:int)->None:
 def processAction(ferry:ship, nav:str):
     action,number = nav[0],int(nav[1:])
     if action == 'F':
-        move(ferry=ferry, action=action, number=number)
+        move(boat=ferry, action=action, number=number)
     elif action == "R" or action== 'L':
         rotate(ferry=ferry, action=action,number=number)
     elif action == 'N':
@@ -54,6 +74,34 @@ def processAction(ferry:ship, nav:str):
         ferry.E+=number
     elif action=='W':
         ferry.W+=number
+
+def processActionToWaywordPoint(ferry:ship,wp:ship, nav:str):
+    action,number = nav[0],int(nav[1:])
+    if action in ['N','E','S','W']:
+        wp.direction = action
+        move(wp,action=action,number=number)
+        wp.setAbsValues()
+
+    elif action in ['R','L']:
+        if number==180:
+            wp.N,wp.S,wp.E,wp.W = wp.S,wp.N,wp.W,wp.E
+        elif (action == "R" and number ==90) or (action == 'L' and number == 270):
+            wp.E, wp.S,wp.W,wp.N=wp.N,wp.E,wp.S,wp.W
+        elif (action == "L" and number ==90) or (action == 'R' and number == 270):
+            wp.N, wp.E, wp.S, wp.W = wp.E, wp.S, wp.W, wp.N
+    
+    elif action == 'F':
+        if wp.N>0:
+            ferry.N+=(wp.N*number)
+        if wp.S>0:
+            ferry.S+=(wp.S*number)
+        if wp.E>0:
+            ferry.E+=(wp.E*number)
+        if wp.W>0:
+            ferry.W+=(wp.W*number)
+        
+        ferry.setAbsValues()
+            
 
 
 def getInput(FilePath:str)->List[str]:
@@ -71,8 +119,21 @@ def p1(navigation:List[str]):
     
     print("Part 1 Answer is: ", ferry.getManhattan())
 
+def p2(navigation:List[str]):
+    ferry=ship()
+    wayward_point = ship()
+    wayward_point.E = 10
+    wayward_point.N = 1
+
+    for nav in navigation:
+        processActionToWaywordPoint(ferry=ferry,wp=wayward_point,nav=nav)
+    
+    print("Part 2 Answer is: ", ferry.getManhattan())
+
 def main():
-    p1(getInput(r"2020\Day_12\input.txt"))
+    navigation = getInput(r"2020\Day_12\input.txt")
+    p1(navigation)
+    p2(navigation)
 
 if __name__ == '__main__':
     main()
