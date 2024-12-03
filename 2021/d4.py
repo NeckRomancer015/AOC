19ea23c0bb303030dfca031d9ed3b8724bb71841
Shankar <49676416+NeckRomancer015@users.noreply.github.com>
2024-12-03 18:06:50 +0200

    2021 d4 p2 fixed

--- 2021/d4.py
+++ 2021/d4.py
@@ -6,11 +6,11 @@ def win_condition(board:list):
 
     for i in range(len(board[0])):
         col = [row[i] for row in board]
-        if col.count('-1') == len(col):
+        if col.count('x') == len(col):
             return True
     
     for i in range(len(board)):
-        if board[i].count('-1') == len(board[i]):
+        if board[i].count('x') == len(board[i]):
             return True
 
 
@@ -19,38 +19,39 @@ def p2(f):
     
     boards = []
     temp = []
-    last_board = []
+    last_score = 0
     for line in common_functions.read_file_line_by_line(f):
         if drawn_numbers == []:
-            drawn_numbers=common_functions.get_numbers_as_list(line)
+            drawn_numbers=common_functions.retrieve_numbers.get_numbers_as_list(line)
         elif line =='':
             if temp  != []:
                 boards.append(temp)
                 temp = []
         else:
-            temp.append(common_functions.get_numbers_as_list(line))
+            temp.append(common_functions.retrieve_numbers.get_numbers_as_list(line))
     boards.append(temp)
     
     boards_won = []
     for bingo in drawn_numbers:
-        for board in boards:
+        for board_num,board in enumerate(boards):            
+            if board_num in boards_won:
+                continue
             for i in range(0,len(board)):
                 for j in range(0,len(board[i])):
                     if bingo == board[i][j]:
-                        board[i][j] = '-1'
+                        board[i][j] = 'x'
                     
-        if win_condition(board):
-            if board in boards_won:
-                continue
-            ans = 0
-            for i in board:
-                for j in i:
-                    if j !='-1':
-                        ans+=int(j)
-            boards_won.append(board)
-            last_board.append(ans*int(bingo))
-    print(last_board[-1])
+            if win_condition(board):
+                ans = 0
+                for i in board:
+                    for j in i:
+                        if j !='x':
+                            ans+=int(j)
+                boards_won.append(board_num)
+                last_score=ans*int(bingo)
+    print(last_score)
     
+                
 
 def p1(f):
     drawn_numbers = []
@@ -59,27 +60,27 @@ def p1(f):
     temp = []
     for line in common_functions.read_file_line_by_line(f):
         if drawn_numbers == []:
-            drawn_numbers=common_functions.get_numbers_as_list(line)
+            drawn_numbers=common_functions.retrieve_numbers.get_numbers_as_list(line)
         elif line =='':
             if temp  != []:
                 boards.append(temp)
                 temp = []
         else:
-            temp.append(common_functions.get_numbers_as_list(line))
+            temp.append(common_functions.retrieve_numbers.get_numbers_as_list(line))
     boards.append(temp)
-    
+
     for bingo in drawn_numbers:
         for board in boards:
             for i in range(0,len(board)):
                 for j in range(0,len(board[i])):
                     if bingo == board[i][j]:
-                        board[i][j] = '-1'
+                        board[i][j] = 'x'
                     
             if win_condition(board):
                 ans = 0
                 for i in board:
                     for j in i:
-                        if j !='-1':
+                        if j !='x':
                             ans+=int(j)
                 print(ans*int(bingo))
                 return
