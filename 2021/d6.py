54b96b9548c3c056879eb9a43d9a8cc0af3e0632
Shankar <49676416+NeckRomancer015@users.noreply.github.com>
2024-12-03 16:45:39 +0200

    2021 d6 p2

--- 2021/d6.py
+++ 2021/d6.py
@@ -23,23 +23,28 @@ def p1(f):
     print(f"part 1 : {len(todays_fish)}")
 
 def p2(f):
-    todays_fish = []
+    todays_fish = {i:0 for i in range(0,9)}
     for line in common_functions.read_file_line_by_line(f):
-        todays_fish = common_functions.retrieve_numbers.get_numbers_as_int_in_list(line)
+        starting_fish = common_functions.retrieve_numbers.get_numbers_as_int_in_list(line)
+        for fish in starting_fish:
+            todays_fish[fish]+=1
+
+    
     
     for i in range(256):
-        tomorrows_fish=''
+        tomorrows_fish={i:0 for i in range(0,9)}
 
-        for fish in todays_fish:
-            if int(fish)>0:
-                val = str(int(fish)-1)
-                tomorrows_fish+=val
-            if int(fish) == 0:
-                tomorrows_fish+='8'
-                tomorrows_fish+='6'
-        todays_fish = re.findall(r'\d',tomorrows_fish)
+        for i in range(8,-1,-1):
+            if i ==0:
+                tomorrows_fish[8] = todays_fish[0]
+                tomorrows_fish[6]+=todays_fish[0]
+            else:
+                tomorrows_fish[i-1]=todays_fish[i]
+        
+        todays_fish = tomorrows_fish
     
-    print(f"part 2 : {len(todays_fish)}")
+    totalfish = sum(todays_fish.values())
+    print(f"part 2 : {totalfish}")
 
 def main():
     f = r"C:\Users\user-pc\Documents\PythonProj\AOC\ex.txt"
